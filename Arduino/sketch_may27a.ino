cc5eade123dfdc41ea95a25601bedf008c486646
bryan <bryancorado777.com>
2022-12-12 17:54:23 -0600

    first commit

---
+++ Arduino/sketch_may27a.ino
@@ -0,0 +1,43 @@
+
+ 
+ #include <Servo.h>
+
+#define pinServo1 10
+#define pinServo2 9
+
+Servo ServoA;
+Servo ServoB;
+
+char comando;
+int posicion;
+
+void setup(){
+  ServoA.attach(10);
+  ServoB.attach(9);
+  ServoA.write(0);
+  ServoB.write(0);
+  
+  Serial.begin(9600);  
+}
+
+void loop(){
+  leer_Dato();
+
+  switch(comando){
+    case 'a':
+      ServoA.write(posicion);
+      comando = ' ';
+      break;
+    case 'b':
+      ServoB.write(posicion);
+      comando = ' ';
+      break;  
+  }
+}
+
+void leer_Dato(){
+  if (Serial.available()>0){
+    comando = Serial.read();
+    posicion = Serial.parseInt();
+    }
+}
